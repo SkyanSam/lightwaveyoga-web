4522bb22ecdd2bef33025197707fdd3cfbcb5cfe
SkyanSam <samuelwiggins15@gmail.com>
2024-07-05 12:21:01 -0400

    phase 2 complete

--- dist/index.b7a05eb9.js
+++ dist/index.b7a05eb9.js
@@ -584,53 +584,50 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"jeorp":[function(require,module,exports) {
+// Importing modules.
 var _tasksVision = require("@mediapipe/tasks-vision");
-let poseLandmarker = undefined;
-let runningMode = "IMAGE";
-let enableWebcamButton;
+// Declaring variables.
+let poseLandmarker;
+let faceLandmarker;
+let runningMode = "VIDEO";
 let webcamRunning = false;
 const videoHeight = "360px";
 const videoWidth = "480px";
-// Before we can use PoseLandmarker class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-const createPoseLandmarker = async ()=>{
-    const vision = await (0, _tasksVision.FilesetResolver).forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+// Function that returns true if a variable is null.
+function isNull(variable) {
+    return variable == null || variable == undefined;
+}
+// Function that loads PoseLandmarker and FaceLandmarker class.
+// Note that this function takes a while, since we are loading the machine learning model.
+const createLandmarkers = async ()=>{
+    const vision = await (0, _tasksVision.FilesetResolver).forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm" // use 0.10.0 or 0.10.3
+    );
     poseLandmarker = await (0, _tasksVision.PoseLandmarker).createFromOptions(vision, {
         baseOptions: {
             modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
             delegate: "GPU"
         },
         runningMode: runningMode,
-        numPoses: 2
+        numPoses: 1
     });
-    // Now that we loaded the pose landmarker we can enable the webcam.
+    faceLandmarker = await (0, _tasksVision.FaceLandmarker).createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+            delegate: "GPU"
+        },
+        outputFaceBlendshapes: true,
+        runningMode: runningMode,
+        numFaces: 1
+    });
+    // Now that we loaded the landmarkers we can enable the webcam.
     const hasGetUserMedia = ()=>!!navigator.mediaDevices?.getUserMedia;
     if (hasGetUserMedia()) enableCam();
     else console.warn("getUserMedia() is not supported by your browser");
 };
-createPoseLandmarker();
-/********************************************************************
-  // Demo 2: Continuously grab image from webcam stream and detect it.
-  ********************************************************************/ const video = document.getElementById("webcam");
-const canvasElement = document.getElementById("output_canvas");
-const canvasCtx = canvasElement.getContext("2d");
-const drawingUtils = new (0, _tasksVision.DrawingUtils)(canvasCtx);
-/*
-  // Check if webcam access is supported.
-  const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
-  
-  // If webcam supported, add event listener to button for when user
-  // wants to activate it.
-  if (hasGetUserMedia()) {
-    enableCam();
-    //enableWebcamButton = document.getElementById("webcamButton");
-    //enableWebcamButton.addEventListener("click", enableCam);
-  } else {
-    console.warn("getUserMedia() is not supported by your browser");
-  }*/ // Enable the live webcam view and start detection.
+// Function that enables the live webcam view and start detection.
 function enableCam() {
-    if (!poseLandmarker) {
+    // Check if the landmarkers have loaded.
+    if (!poseLandmarker || !faceLandmarker) {
         console.log("Wait! poseLandmaker not loaded yet.");
         return;
     }
@@ -646,13 +643,20 @@ function enableCam() {
         video.addEventListener("loadeddata", predictWebcam);
     });
 }
+// Call the asynchronous function.
+createLandmarkers();
+// Get video and canvas contexts.
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+// Function that is called whenever video stream recieves new data, (so every frame).
 let lastVideoTime = -1;
 async function predictWebcam() {
     canvasElement.style.height = videoHeight;
     video.style.height = videoHeight;
     canvasElement.style.width = videoWidth;
     video.style.width = videoWidth;
-    // Now let's start detecting the stream.
+    // Detecting the stream.
     if (runningMode === "IMAGE") {
         runningMode = "VIDEO";
         await poseLandmarker.setOptions({
@@ -660,27 +664,27 @@ async function predictWebcam() {
         });
     }
     let startTimeMs = performance.now();
+    let poseLandmarks = [];
+    let faceLandmarks = [];
     if (lastVideoTime !== video.currentTime) {
         lastVideoTime = video.currentTime;
+        // Get landmarks from pose model.
         poseLandmarker.detectForVideo(video, startTimeMs, (result)=>{
             canvasCtx.save();
             canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-            for (const landmark of result.landmarks){
-                console.log("Landmark");
-                drawChakras(landmark);
-                window.landmark = landmark;
-                // draw landmarks
-                drawingUtils.drawLandmarks(landmark, {
-                    radius: (data)=>(0, _tasksVision.DrawingUtils).lerp(data.from.z, -0.15, 0.1, 5, 1)
-                });
-                drawingUtils.drawConnectors(landmark, (0, _tasksVision.PoseLandmarker).POSE_CONNECTIONS);
-            }
+            for (const landmark of result.landmarks)poseLandmarks = landmark;
             canvasCtx.restore();
         });
+        // Get landmarks from face model.
+        let results = faceLandmarker.detectForVideo(video, startTimeMs);
+        if (results.faceLandmarks) for (const landmarks of results.faceLandmarks)faceLandmarks = landmarks;
+        // Draw the chakras.
+        drawChakras(poseLandmarks, faceLandmarks);
     }
     // Call this function again to keep predicting when the browser is ready.
     if (webcamRunning) requestAnimationFrame(predictWebcam);
 }
+// Function that performs interpolation between two points.
 function lerp(p1, p2, t) {
     let p_result = {
         x: (p2.x - p1.x) * t + p1.x,
@@ -689,25 +693,54 @@ function lerp(p1, p2, t) {
     };
     return p_result;
 }
-function drawChakras(landmark) {
+// Function that draws the chakras.
+function drawChakras(landmark, faceLandmark) {
+    // Checks to make sure the landmark list is not undefined.
+    // Also checks that the landmarks that we will be using are not undefined.
+    if (isNull(landmark)) return;
+    let landmarks_used = [
+        1,
+        4,
+        11,
+        12,
+        23,
+        24
+    ];
+    for(var l in landmarks_used){
+        if (isNull(landmark[l])) return;
+    }
+    // Get chakra coordinates using interpolation as necessary.
+    let top_head_pt = faceLandmark[10];
     let bottom_spine_pt = lerp(landmark[23], landmark[24], 0.5);
     let top_spine_pt = lerp(landmark[12], landmark[11], 0.5);
     let third_eye_chakra_pt = lerp(landmark[1], landmark[4], 0.5);
-    let webcam = document.getElementById("webcam");
+    // Creating arrays for chakra html elements and chakra coordinates.
     let chakras = [];
     let chakra_points = [];
-    for(let i = 0; i < 6; i++){
+    // Getting HTML elements
+    let webcam = document.getElementById("webcam");
+    for(let i = 0; i < 7; i++){
         let chakra = document.getElementById("chakra" + String(i));
-        if (chakra != null) chakras.push(chakra);
+        if (!isNull(chakra)) chakras.push(chakra);
     }
+    // Interpolating chakra coordinates from the root chakra to the throat chakra.
+    let p1 = lerp(bottom_spine_pt, top_spine_pt, -0.1);
+    let p2 = lerp(bottom_spine_pt, top_spine_pt, 1.1);
     for(let i = 0; i <= 4; i++){
-        let p1 = lerp(bottom_spine_pt, top_spine_pt, -0.1);
-        let p2 = lerp(bottom_spine_pt, top_spine_pt, 1.1);
         let chakra_point = lerp(p1, p2, i / 4.0);
         chakra_points.push(chakra_point);
     }
+    // Adding third eye Chakra coordinates.
     chakra_points.push(third_eye_chakra_pt);
-    for(let i = 0; i < 6; i++)if (chakras[i] != null && webcam != null) {
+    // Add crown chakra coordinates if the face landmark exists.
+    if (!isNull(faceLandmark) && !isNull(top_head_pt)) chakra_points.push(lerp(chakra_points[4], top_head_pt, 1.05));
+    else chakra_points.push({
+        x: 0,
+        y: 0,
+        z: 0
+    });
+    // Update chakra HTML elements and their positions.
+    for(let i = 0; i < 7; i++)if (!isNull(chakras[i]) && !isNull(chakra_points[i]) && !isNull(webcam)) {
         chakras[i].style.position = "absolute";
         let cam_width = parseInt(webcam.style.width);
         let cam_height = parseInt(webcam.style.height);
@@ -720,9 +753,6 @@ function drawChakras(landmark) {
             chakras[i].style.top = top_pos + "px";
         }
     }
-// try one and see how it goes!!
-//let throat_chakra_pt = lerp(bottom_spine_pt, top_spine_pt, 1.1);
-//let heart_chakra_pt = lerp(bot)
 }
 
 },{"@mediapipe/tasks-vision":"e5Mjq"}],"e5Mjq":[function(require,module,exports) {
